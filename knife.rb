d0ce2268561dff7e390f1a02e9b81fb038236139
Your Name <you@example.com>
2017-11-13 13:20:19 +0000

    all the chef configuration files

---
+++ knife.rb
@@ -0,0 +1,7 @@
+node_name                'krishna'
+client_key               '/home/krishna/growuniversal/.chef/krishna.pem'
+validation_client_name   'growuniversal-validator'
+validation_key           '/home/krishna/growuniversal/.chef/growuniversal-validator.pem'
+chef_server_url          'https://chef-server/organizations/growuniversal'
+syntax_check_cache_path  '/home/krishna/growuniversal/.chef/syntax_check_cache'
+cookbook_path [ '~/growuniversal/cookbooks' ]
